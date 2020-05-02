fba4c570b5ed120cca3eb632d1b55d8a9d3f6d8e
Louis Thibault <l.thibault@sentimens.com>
2020-05-02 20:23:19 +0100

    HACK/BUGFIX:  patch a stochastic panic out of isClient.  This is               a temporary fix until libp2p emits PeerConnectednessChanged

--- pkg/server/eventloop.go
+++ pkg/server/eventloop.go
@@ -16,8 +16,8 @@ import (
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	peerstore "github.com/libp2p/go-libp2p-peerstore"
-	"github.com/pkg/errors"
 
+	log "github.com/lthibault/log/pkg"
 	ww "github.com/lthibault/wetware/pkg"
 )
 
@@ -26,6 +26,7 @@ const uagentKey = "AgentVersion"
 type evtLoopConfig struct {
 	fx.In
 
+	Log  log.Logger
 	Host host.Host
 	KMin int `name:"kmin"`
 	KMax int `name:"kmax"`
@@ -33,7 +34,7 @@ type evtLoopConfig struct {
 
 // main event loop
 func eventloop(lx fx.Lifecycle, cfg evtLoopConfig) (err error) {
-	if err = dispatchNetworkEvts(lx, cfg.Host); err != nil {
+	if err = dispatchNetworkEvts(lx, cfg.Log, cfg.Host); err != nil {
 		return
 	}
 
@@ -45,7 +46,7 @@ func eventloop(lx fx.Lifecycle, cfg evtLoopConfig) (err error) {
 //
 // HACK:  This is a short-term solution while we wait for libp2p to provide equivalent
 //		  functionality.
-func dispatchNetworkEvts(lx fx.Lifecycle, host host.Host) error {
+func dispatchNetworkEvts(lx fx.Lifecycle, log log.Logger, host host.Host) error {
 	bus := host.EventBus()
 
 	connEvt, err := bus.Emitter(new(ww.EvtConnectionChanged))
@@ -68,7 +69,7 @@ func dispatchNetworkEvts(lx fx.Lifecycle, host host.Host) error {
 			ev := v.(event.EvtPeerIdentificationCompleted)
 			connEvt.Emit(ww.EvtConnectionChanged{
 				Peer:   ev.Peer,
-				Client: isClient(ev.Peer, host.Peerstore()),
+				Client: isClient(log, ev.Peer, host.Peerstore()),
 				State:  ww.ConnStateOpened,
 			})
 		}
@@ -78,7 +79,7 @@ func dispatchNetworkEvts(lx fx.Lifecycle, host host.Host) error {
 		// NOTE:  can't use ConnectedF because the
 		//		  identity protocol will not have
 		// 		  completed, meaning isClient will panic.
-		DisconnectedF: onDisconnected(connEvt, host.Peerstore()),
+		DisconnectedF: onDisconnected(log, connEvt, host.Peerstore()),
 
 		OpenedStreamF: onStreamOpened(strmEvt),
 		ClosedStreamF: onStreamClosed(strmEvt),
@@ -96,11 +97,11 @@ func dispatchNetworkEvts(lx fx.Lifecycle, host host.Host) error {
 	return nil
 }
 
-func onDisconnected(e event.Emitter, m peerstore.PeerMetadata) func(network.Network, network.Conn) {
+func onDisconnected(log log.Logger, e event.Emitter, m peerstore.PeerMetadata) func(network.Network, network.Conn) {
 	return func(net network.Network, conn network.Conn) {
 		e.Emit(ww.EvtConnectionChanged{
 			Peer:   conn.RemotePeer(),
-			Client: isClient(conn.RemotePeer(), m),
+			Client: isClient(log, conn.RemotePeer(), m),
 			State:  ww.ConnStateClosed,
 		})
 	}
@@ -216,8 +217,9 @@ func (n neighborhood) Add(id peer.ID) (leased bool) {
 func (n neighborhood) Rm(id peer.ID) (evicted bool) {
 	i, ok := n.m[id]
 	if !ok {
-		// ok == false implies a client disconnected; should never happen.
-		panic("unreachable")
+		// if we ever hit this (and it's _actually_ isClient), consider simply removing
+		// this test and returning false.
+		panic("unreachable - probably caused by isClient")
 	}
 
 	if i == 1 {
@@ -254,14 +256,50 @@ func (n neighborhood) Phase() ww.Phase {
 //
 // Developers should prefer to work at the host level, comparing peer.IDs in the
 // peerstore to those in the filter/routing-table by means of `filter.Contains`.
-func isClient(p peer.ID, ps peerstore.PeerMetadata) bool {
+func isClient(log log.Logger, p peer.ID, ps peerstore.PeerMetadata) bool {
 	switch v, err := ps.Get(p, uagentKey); err {
 	case nil:
 		return v.(string) == ww.ClientUAgent
 	case peerstore.ErrNotFound:
 		// This usually means isClient was called in network.Notifiee.Connected, before
 		// authentication by the IDService completed.
-		panic(errors.Wrap(err, "user agent not found"))
+
+		// XXX: this is stochastically triggered with the following log output appearing
+		//      immediately before the panic stack-trace appears.
+		//
+		//      Best guess:
+		//			1.  connection established
+		//			2.  id stream opened
+		//			3.  something goes wrong ==> stream reset ==> conn closed
+		//			4.  onConnClosed triggered, but event.PeerIdentificationCompleted
+		//				never fired ==> user agent not present ==> panic.
+		//
+		//		MITIGATION:  emit error-level log message instead of panicking.
+
+		/*
+			TRAC[0001] peer identification failed
+
+			addrs="[/ip4/127.0.0.1/tcp/64725 /ip6/::1/tcp/64726]"
+			error="stream reset"
+			id=QmXJjG9TZzrmQV419v2vcVoGuv15U3RreYTb1b8js9S2id
+			ns=ww
+			peer=QmNzXbNoCdWpYKiYKv2VEBVDh21uxoYQ5Pxcck1uxZYzte
+
+			panic: user agent not found: item not found
+		*/
+
+		log.WithError(err).Error("isClient failed to get user agent")
+
+		// HACK: so far we've only observed this in host-host connections (though we've
+		// never tested a host-client conn).
+		//
+		// False positives (clients misidentified as hosts) will trigger a panic in
+		// neighborhood.Rm.  If this happens, consider simply removing the panic
+		// statement in `Rm`.  We _probably_ don't need it.
+		//
+		// Either way, this is a tempororary hack until upstream libp2p begins emitting
+		// event.PeerConnectednessChanged.
+		return true
 	default:
 		panic(err)
 	}
