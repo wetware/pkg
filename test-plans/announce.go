aaf77b5bafbb663b1e28626aba3bc41dd03ae9bd
Louis Thibault <l.thibault@sentimens.com>
2020-05-03 20:58:50 +0100

    Add announce test plan

--- test-plans/announce.go
+++ test-plans/announce.go
@@ -1,11 +1,73 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	peer "github.com/libp2p/go-libp2p-peer"
+	"github.com/pkg/errors"
+	"github.com/testground/sdk-go/network"
 	"github.com/testground/sdk-go/runtime"
+	"github.com/testground/sdk-go/sync"
+
+	"github.com/lthibault/wetware/pkg/server"
 )
 
 // TestAnnounce verifies that hosts are mutually aware of each others' presence.
-func TestAnnounce(runenv *runtime.RunEnv) error {
+func TestAnnounce(runenv *runtime.RunEnv) (err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
+	defer cancel()
 
-	return nil
+	client := sync.MustBoundClient(ctx, runenv)
+	defer client.Close()
+
+	netclient := network.NewClient(client, runenv)
+	runenv.RecordMessage("Waiting for network initialization")
+
+	netclient.MustWaitNetworkInitialized(ctx)
+	runenv.RecordMessage("Network initilization complete")
+
+	host := server.New(
+		server.WithTTL(time.Millisecond*100),
+		server.WithDiscover(SyncProto{
+			Client: client,
+			N:      runenv.TestInstanceCount,
+		}),
+	)
+
+	if err = host.Start(); err != nil {
+		return errors.Wrap(err, "start host")
+	}
+
+	select {
+	case <-time.After(time.Second):
+		// heartbeats should have propagated
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
+	// tests proper
+	switch peers := host.Peers(); {
+	case len(peers) != runenv.TestInstanceCount:
+		msg := fmt.Sprintf("expected %d peers, found %d", runenv.TestInstanceCount, len(peers))
+		runenv.SLogger().Error(msg)
+		err = errors.New(msg)
+	case !contains(peers, host.ID()):
+		msg := fmt.Sprintf("%s not in peer set", host.ID())
+		runenv.SLogger().Error(msg)
+		err = errors.New(msg)
+	}
+
+	return err
+}
+
+func contains(ps []peer.ID, p peer.ID) bool {
+	set := make(map[peer.ID]struct{})
+	for _, px := range ps {
+		set[px] = struct{}{}
+	}
+
+	_, ok := set[p]
+	return ok
 }
--- test-plans/go.mod
+++ test-plans/go.mod
@@ -3,9 +3,9 @@ module github.com/lthibault/wetware/test-plans
 go 1.14
 
 require (
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/libp2p/go-libp2p-core v0.5.3
+	github.com/libp2p/go-libp2p-peer v0.2.0
+	github.com/lthibault/wetware v0.0.0-20200503135817-3ba54165d31b
+	github.com/pkg/errors v0.9.1
 	github.com/testground/sdk-go v0.2.0
-	golang.org/x/lint v0.0.0-20200130185559-910be7a94367 // indirect
-	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
