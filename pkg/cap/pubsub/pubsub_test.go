dc61c560f93ccf69fe7794f1c6c5aa9b20ae1971
Louis Thibault <l.thibault@sentimens.com>
2022-01-28 12:21:54 -0500

    Test concurrent publish/subscribe.

--- pkg/cap/pubsub/pubsub_test.go
+++ pkg/cap/pubsub/pubsub_test.go
@@ -2,9 +2,11 @@ package pubsub_test
 
 import (
 	"context"
+	"sync"
 	"testing"
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	syncutil "github.com/lthibault/util/sync"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -47,4 +49,60 @@ func TestPubSub(t *testing.T) {
 	b, err := sub.Next(ctx)
 	require.NoError(t, err, "Next() should succeed")
 	assert.Equal(t, "test", string(b), "message should contain 'test'")
+}
+
+func TestPubSub_concurrent(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sim := mx.New(ctx)
+
+	h := sim.MustHost(ctx)
+
+	gs, err := pubsub.NewFloodSub(ctx, h)
+	require.NoError(t, err)
+
+	f := pscap.New(gs)
+	defer f.Close()
+
+	const n = 32
+	var wg sync.WaitGroup
+	var b = syncutil.NewBarrierChan(n)
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+
+			ps := f.New(nil)
+			defer ps.Release()
+
+			f, release := ps.Join(ctx, "test")
+			defer release()
+
+			sub := f.Topic().Subscribe()
+			assert.NotNil(t, sub, "should always return non-nil subscription")
+			defer sub.Cancel()
+
+			err := sub.Resolve(ctx)
+			assert.NoError(t, err, "should resolve successfully")
+
+			// Wait for all goroutines to resolve their subscriptions,
+			// so that nobody misses a message.
+			b.SignalAndWait(func() {
+				t.Log("all topics resolved")
+			})
+
+			err = f.Topic().Publish(ctx, []byte("test"))
+			assert.NoError(t, err, "publish should succeed")
+
+			b, err := sub.Next(ctx)
+			assert.NoError(t, err, "Next() should succeed")
+			assert.Equal(t, "test", string(b), "message should contain 'test'")
+		}()
+	}
+
+	wg.Wait()
 }
