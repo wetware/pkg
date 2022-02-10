d6400f5422cfcca9eac76186fee3cb600d1754a4
Louis Thibault <l.thibault@sentimens.com>
2022-02-10 18:23:40 -0500

    Clean up bucket names for statsd bandwidth reporter.

--- internal/util/statsd/reporter.go
+++ internal/util/statsd/reporter.go
@@ -17,21 +17,27 @@ func NewBandwidthCounter(s *statsd.Client) (b *metrics.BandwidthCounter, stop fu
 
 	ticker := time.NewTicker(time.Minute) // 1440 samples/day
 	go func() {
+		stat := b.GetBandwidthTotals()
+		s.Gauge("rate.in", stat.RateIn)
+		s.Gauge("rate.out", stat.RateOut)
+		s.Gauge("total.in", stat.TotalIn)
+		s.Gauge("total.out", stat.TotalOut)
+
 		for range ticker.C {
 			for id, stat := range b.GetBandwidthByPeer() {
 				prefix := fmt.Sprintf("peer.%s.", id)
-				s.Gauge(prefix+"rate_in", stat.RateIn)
-				s.Gauge(prefix+"rate_out", stat.RateOut)
-				s.Gauge(prefix+"total_in", stat.TotalIn)
-				s.Gauge(prefix+"total_out", stat.TotalOut)
+				s.Gauge(prefix+"rate.in", stat.RateIn)
+				s.Gauge(prefix+"rate.out", stat.RateOut)
+				s.Gauge(prefix+"total.in", stat.TotalIn)
+				s.Gauge(prefix+"total.out", stat.TotalOut)
 			}
 
 			for proto, stat := range b.GetBandwidthByProtocol() {
 				prefix := fmt.Sprintf("proto.%s.", proto)
-				s.Gauge(prefix+"rate_in", stat.RateIn)
-				s.Gauge(prefix+"rate_out", stat.RateOut)
-				s.Gauge(prefix+"total_in", stat.TotalIn)
-				s.Gauge(prefix+"total_out", stat.TotalOut)
+				s.Gauge(prefix+"rate.in", stat.RateIn)
+				s.Gauge(prefix+"rate.out", stat.RateOut)
+				s.Gauge(prefix+"total.in", stat.TotalIn)
+				s.Gauge(prefix+"total.out", stat.TotalOut)
 			}
 		}
 	}()
