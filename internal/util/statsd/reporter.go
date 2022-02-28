93fac3fc0920de8e57f534c6074af65281d0e9c3
Louis Thibault <l.thibault@sentimens.com>
2022-02-28 16:22:49 -0500

    Reduce verbosity of default BandwidthCounter

--- internal/util/statsd/reporter.go
+++ internal/util/statsd/reporter.go
@@ -12,32 +12,19 @@ func NewBandwidthCounter(s *statsd.Client) (b *metrics.BandwidthCounter, stop fu
 
 	b = metrics.NewBandwidthCounter()
 	s.Clone(
-		statsd.SampleRate(1.), // send 100% of metrics
+		statsd.SampleRate(.1), // send 10% of metrics
 		statsd.Prefix("libp2p.host.bandwidth."))
 
-	ticker := time.NewTicker(time.Minute) // 1440 samples/day
+	ticker := time.NewTicker(time.Minute) // 1440 samples/day base-rate
 	go func() {
 		stat := b.GetBandwidthTotals()
 		s.Gauge("rate.in", stat.RateIn)
 		s.Gauge("rate.out", stat.RateOut)
-		s.Gauge("total.in", stat.TotalIn)
-		s.Gauge("total.out", stat.TotalOut)
 
 		for range ticker.C {
-			for id, stat := range b.GetBandwidthByPeer() {
-				prefix := fmt.Sprintf("peer.%s.", id)
-				s.Gauge(prefix+"rate.in", stat.RateIn)
-				s.Gauge(prefix+"rate.out", stat.RateOut)
-				s.Gauge(prefix+"total.in", stat.TotalIn)
-				s.Gauge(prefix+"total.out", stat.TotalOut)
-			}
-
 			for proto, stat := range b.GetBandwidthByProtocol() {
-				prefix := fmt.Sprintf("proto.%s.", proto)
-				s.Gauge(prefix+"rate.in", stat.RateIn)
-				s.Gauge(prefix+"rate.out", stat.RateOut)
-				s.Gauge(prefix+"total.in", stat.TotalIn)
-				s.Gauge(prefix+"total.out", stat.TotalOut)
+				s.Gauge(fmt.Sprintf("%s.rate.in", proto), stat.RateIn)
+				s.Gauge(fmt.Sprintf("%s.rate.out", proto), stat.RateOut)
 			}
 		}
 	}()
