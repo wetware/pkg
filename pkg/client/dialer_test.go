baaff6c64250220cec2a75eff7e82b7f88a70a21
Louis Thibault <l.thibault@sentimens.com>
2022-03-20 16:12:49 -0400

    Move client 'integration' test into dialer tests.

--- pkg/client/dialer_test.go
+++ pkg/client/dialer_test.go
@@ -2,8 +2,10 @@ package client_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	"capnproto.org/go/capnp/v3"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -15,6 +17,10 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/wetware/casm/pkg/boot"
+	clapi "github.com/wetware/ww/internal/api/cluster"
+	psapi "github.com/wetware/ww/internal/api/pubsub"
+	"github.com/wetware/ww/pkg/cap/cluster"
+	"github.com/wetware/ww/pkg/cap/pubsub"
 	"github.com/wetware/ww/pkg/client"
 	"github.com/wetware/ww/pkg/vat"
 )
@@ -104,6 +110,66 @@ func TestDialer(t *testing.T) {
 		assert.EqualError(t, err, "protocol not supported")
 		assert.Nil(t, n, "should return nil client node")
 	})
+
+	t.Run("Success", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		h, err := libp2p.New(
+			libp2p.NoListenAddrs,
+			libp2p.NoTransports,
+			libp2p.ListenAddrStrings("/inproc/~"),
+			libp2p.Transport(inproc.New()))
+		require.NoError(t, err, "must succeed")
+		defer h.Close()
+
+		svr := vat.Network{
+			NS:   "test",
+			Host: h,
+		}
+		svr.Export(pubsub.Capability, mockPubSub{})
+		svr.Export(cluster.ViewCapability, mockView{})
+
+		clt := newVat()
+		defer clt.Host.Close()
+
+		n, err := client.Dialer{
+			Vat:  clt,
+			Boot: boot.StaticAddrs{*host.InfoFromHost(h)},
+		}.Dial(ctx)
+
+		assert.NoError(t, err, "should return without error")
+		assert.NotNil(t, n, "should return non-nil node")
+
+		err = n.Bootstrap(ctx)
+		assert.NoError(t, err, "should bootstrap successfully")
+	})
+}
+
+type mockPubSub struct{}
+
+func (mockPubSub) Join(ctx context.Context, call psapi.PubSub_join) error {
+	return fmt.Errorf("NOT IMPLEMENTED")
+}
+
+func (mockPubSub) Client() *capnp.Client {
+	return psapi.PubSub_ServerToClient(mockPubSub{}, nil).Client
+}
+
+type mockView struct{}
+
+func (mockView) Client() *capnp.Client {
+	return clapi.View_ServerToClient(mockView{}, nil).Client
+}
+
+func (mockView) Iter(ctx context.Context, call clapi.View_iter) error {
+	return fmt.Errorf("NOT IMPLEMENTED")
+}
+
+func (mockView) Lookup(ctx context.Context, call clapi.View_lookup) error {
+	return fmt.Errorf("NOT IMPLEMENTED")
 }
 
 func newVat() vat.Network {
