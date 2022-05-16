000e314160b9b0f837036d7e1036b4ca82b341a0
Louis Thibault <l.thibault@sentimens.com>
2022-05-16 14:42:11 -0400

    Add refcounting test for closing of subscription channel.

--- pkg/cap/pubsub/subscription_test.go
+++ pkg/cap/pubsub/subscription_test.go
@@ -9,6 +9,57 @@ import (
 	api "github.com/wetware/ww/internal/api/pubsub"
 )
 
+func TestSubscription_refcount(t *testing.T) {
+	t.Parallel()
+
+	/*
+	 *  Checks that releasing a Topic causes all handlers to be
+	 *  released, and that this in turn closes the subscription
+	 *  and its channel.
+	 */
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	topic := Topic(api.Topic_ServerToClient(mockTopic{}, nil))
+	defer topic.Release()
+
+	ctx = context.WithValue(ctx, keyHandlerCallback{},
+		handlerCallback(func(h api.Topic_Handler) error {
+			f, release := h.Handle(ctx, func(ps api.Topic_Handler_handle_Params) error {
+				return ps.SetMsg([]byte("hello, world"))
+			})
+			defer release()
+
+			_, err := f.Struct()
+			return err
+		}))
+
+	ch := make(chan []byte, 1)
+
+	release, err := topic.Subscribe(ctx, ch)
+	require.NoError(t, err)
+	defer release()
+
+	// Ensure we have a message in the subscription channel, then release
+	// the topic.  This MUST cause the subscription channel to be closed,
+	// but we should still be able to read the buffered message.
+	require.Len(t, ch, 1, "message should be buffered in subscription channel")
+	topic.Release()
+
+	var got []byte
+	select {
+	case got = <-ch:
+	default:
+	}
+
+	assert.Equal(t, []byte("hello, world"), got,
+		"should receive message")
+
+	_, ok := <-ch
+	require.False(t, ok, "channel should be closed")
+}
+
 func TestHandler(t *testing.T) {
 	t.Parallel()
 	t.Helper()
@@ -60,3 +111,32 @@ func TestHandler(t *testing.T) {
 		assert.False(t, ok, "should close message channel when released")
 	})
 }
+
+type mockTopic struct{}
+
+func (mockTopic) Publish(ctx context.Context, call api.Topic_publish) error {
+	handle := ctx.Value(keyPublishCallback{}).(publishCallback)
+	return handle(call.Args())
+}
+
+func (mockTopic) Subscribe(ctx context.Context, call api.Topic_subscribe) error {
+	handle := ctx.Value(keyHandlerCallback{}).(handlerCallback)
+	return handle(call.Args().Handler())
+}
+
+func (mockTopic) Name(ctx context.Context, call api.Topic_name) error {
+	res, err := call.AllocResults()
+	if err != nil {
+		return err
+	}
+
+	return res.SetName(ctx.Value(keyName{}).(string))
+}
+
+type publishCallback func(args interface{ Msg() ([]byte, error) }) error
+type keyPublishCallback struct{}
+
+type handlerCallback func(h api.Topic_Handler) error
+type keyHandlerCallback struct{}
+
+type keyName struct{}
