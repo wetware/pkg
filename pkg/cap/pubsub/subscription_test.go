b4ba291f6f4cec1a03ed3b836d18dcc722f710e7
Louis Thibault <l.thibault@sentimens.com>
2022-05-16 14:55:22 -0400

    Replace TestHandler with another TestSubscription_refcount case.

--- pkg/cap/pubsub/subscription_test.go
+++ pkg/cap/pubsub/subscription_test.go
@@ -11,6 +11,7 @@ import (
 
 func TestSubscription_refcount(t *testing.T) {
 	t.Parallel()
+	t.Helper()
 
 	/*
 	 *  Checks that releasing a Topic causes all handlers to be
@@ -18,97 +19,90 @@ func TestSubscription_refcount(t *testing.T) {
 	 *  and its channel.
 	 */
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	topic := Topic(api.Topic_ServerToClient(mockTopic{}, nil))
-	defer topic.Release()
-
-	ctx = context.WithValue(ctx, keyHandlerCallback{},
-		handlerCallback(func(h api.Topic_Handler) error {
-			f, release := h.Handle(ctx, func(ps api.Topic_Handler_handle_Params) error {
-				return ps.SetMsg([]byte("hello, world"))
-			})
-			defer release()
-
-			_, err := f.Struct()
-			return err
-		}))
-
-	ch := make(chan []byte, 1)
-
-	release, err := topic.Subscribe(ctx, ch)
-	require.NoError(t, err)
-	defer release()
-
-	// Ensure we have a message in the subscription channel, then release
-	// the topic.  This MUST cause the subscription channel to be closed,
-	// but we should still be able to read the buffered message.
-	require.Len(t, ch, 1, "message should be buffered in subscription channel")
-	topic.Release()
-
-	var got []byte
-	select {
-	case got = <-ch:
-	default:
-	}
-
-	assert.Equal(t, []byte("hello, world"), got,
-		"should receive message")
-
-	_, ok := <-ch
-	require.False(t, ok, "channel should be closed")
-}
-
-func TestHandler(t *testing.T) {
-	t.Parallel()
-	t.Helper()
-
-	t.Run("Handle", func(t *testing.T) {
+	t.Run("HandlerClosesChannel", func(t *testing.T) {
 		t.Parallel()
 
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
-		ms := make(chan []byte, 1)
-		h := handler{
-			ms:      ms,
-			release: func() {},
-		}
+		topic := Topic(api.Topic_ServerToClient(mockTopic{}, nil))
+		defer topic.Release()
 
-		c := api.Topic_Handler_ServerToClient(h, nil)
-		defer c.Release()
+		ctx = context.WithValue(ctx, keyHandlerCallback{},
+			handlerCallback(func(h api.Topic_Handler) error {
+				// Release the handler AFTER we have written a message to the
+				// subscription channel.
+				defer h.Release()
 
-		f, release := c.Handle(ctx, func(ps api.Topic_Handler_handle_Params) error {
-			return ps.SetMsg([]byte("test"))
-		})
+				f, release := h.Handle(ctx, func(ps api.Topic_Handler_handle_Params) error {
+					return ps.SetMsg([]byte("hello, world"))
+				})
+				defer release()
+
+				_, err := f.Struct()
+				return err
+			}))
+
+		ch := make(chan []byte, 1)
+
+		release, err := topic.Subscribe(ctx, ch)
+		require.NoError(t, err)
 		defer release()
 
-		_, err := f.Struct()
-		assert.NoError(t, err, "call to Handle should succeed")
-		assert.Equal(t, "test", string(<-ms), "unexpected message")
+		// Ensure we have a message in the subscription channel. The
+		// handler should have already been closed, but we should be
+		// able to read the buffered message.
+		require.Len(t, ch, 1,
+			"message should be buffered in subscription channel")
+
+		assert.Equal(t, []byte("hello, world"), <-ch,
+			"should receive message")
+
+		_, ok := <-ch
+		require.False(t, ok, "channel should be closed")
 	})
 
-	t.Run("Release", func(t *testing.T) {
+	t.Run("TopicReleasesHandler", func(t *testing.T) {
 		t.Parallel()
 
-		var (
-			called bool
-			ms     = make(chan []byte, 1)
-		)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-		h := handler{
-			ms:      ms,
-			release: func() { called = true },
-		}
+		topic := Topic(api.Topic_ServerToClient(mockTopic{}, nil))
+		defer topic.Release()
 
-		c := api.Topic_Handler_ServerToClient(h, nil)
-		c.Release()
+		ctx = context.WithValue(ctx, keyHandlerCallback{},
+			handlerCallback(func(h api.Topic_Handler) error {
+				f, release := h.Handle(ctx, func(ps api.Topic_Handler_handle_Params) error {
+					return ps.SetMsg([]byte("hello, world"))
+				})
+				defer release()
 
-		require.True(t, called, "should call release function")
+				_, err := f.Struct()
+				return err
+			}))
 
-		_, ok := <-ms
-		assert.False(t, ok, "should close message channel when released")
+		ch := make(chan []byte, 1)
+
+		release, err := topic.Subscribe(ctx, ch)
+		require.NoError(t, err)
+		defer release()
+
+		// Release the topic AFTER we have written a message to the
+		// subscription channel.
+		topic.Release()
+
+		// Ensure we have a message in the subscription channel. The
+		// handler should have already been closed, but we should be
+		// able to read the buffered message.
+		require.Len(t, ch, 1,
+			"message should be buffered in subscription channel")
+
+		assert.Equal(t, []byte("hello, world"), <-ch,
+			"should receive message")
+
+		_, ok := <-ch
+		require.False(t, ok, "channel should be closed")
 	})
 }
 
