cee3fb5b75e709e85c1160c2149b60d90e0038b9
Louis Thibault <l.thibault@sentimens.com>
2022-06-09 11:18:46 -0400

    Move writerFunc to stdio.go

--- pkg/ocap/process/unix/stdio.go
+++ pkg/ocap/process/unix/stdio.go
@@ -185,3 +185,11 @@ func (sw *swriter) close() (err error) {
 
 	return
 }
+
+// writerFunc is a function type that implements io.Writer.
+type writerFunc func([]byte) (int, error)
+
+// Write calls the function with b as its argument.
+func (write writerFunc) Write(b []byte) (int, error) {
+	return write(b)
+}
--- pkg/ocap/process/unix/util.go
+++
@@ -1,9 +0,0 @@
-package unix
-
-// writerFunc is a function type that implements io.Writer.
-type writerFunc func([]byte) (int, error)
-
-// Write calls the function with b as its argument.
-func (write writerFunc) Write(b []byte) (int, error) {
-	return write(b)
-}
