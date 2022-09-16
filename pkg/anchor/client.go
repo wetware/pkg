832cfbabd596573d2a518f504e9c8b97fa304e56
Louis Thibault <l.thibault@sentimens.com>
2022-09-16 16:59:38 -0400

    Implement Anchor client.

--- pkg/anchor/client.go
+++ pkg/anchor/client.go
@@ -8,37 +8,29 @@ import (
 	api "github.com/wetware/ww/internal/api/anchor"
 )
 
-type Anchor interface {
-	String() string
-	Path() Path
+// type Anchor interface {
+// 	String() string
+// 	Path() Path
 
-	Ls(context.Context) (Iterator, capnp.ReleaseFunc)
-	Walk(context.Context, string) Anchor
+// 	Ls(context.Context) (Iterator, capnp.ReleaseFunc)
+// 	Walk(context.Context, string) Anchor
 
-	Client() capnp.Client
-	AddRef() Anchor
-	Release()
-}
-
-// type client struct {
-// 	path Path
-// 	api.Anchor
+// 	Client() capnp.Client
+// 	AddRef() Anchor
+// 	Release()
 // }
 
-type Client struct {
-	api.Anchor
-	path Path
+type Anchor api.Anchor
+
+func (a Anchor) AddRef() Anchor {
+	return Anchor(api.Anchor(a).AddRef())
 }
 
-func (a Client) AddRef() Client {
-	return Client(api.Anchor(a).AddRef())
-}
-
-func (a Client) Release() {
+func (a Anchor) Release() {
 	capnp.Client(a).Release()
 }
 
-func (a Client) Ls(ctx context.Context) (Iterator, capnp.ReleaseFunc) {
+func (a Anchor) Ls(ctx context.Context) (Iterator, capnp.ReleaseFunc) {
 	f, release := api.Anchor(a).Ls(ctx, nil)
 	h := &handler{Future: casm.Future(f)}
 	return Iterator{
@@ -48,11 +40,18 @@ func (a Client) Ls(ctx context.Context) (Iterator, capnp.ReleaseFunc) {
 }
 
 // Walk to the register located at path.
-func (a Client) Walk(ctx context.Context, path string) (Anchor, capnp.ReleaseFunc) {
-	return walkPath(ctx, api.Anchor(a), path)
+func (a Anchor) Walk(ctx context.Context, path string) (Anchor, capnp.ReleaseFunc) {
+	p := NewPath(path)
+
+	if p.IsRoot() {
+		return Anchor(a), a.AddRef().Release
+	}
+
+	f, release := api.Anchor(a).Walk(ctx, walkParam(p))
+	return Anchor(f.Anchor()), release
 }
 
-type Iterator casm.Iterator[Client]
+type Iterator casm.Iterator[Anchor]
 
 type handler struct {
 	casm.Future
@@ -83,19 +82,19 @@ func (h *handler) Next() (a Anchor, ok bool) {
 func (h *handler) anchor(i int) (Anchor, bool) {
 	res, ok := h.results()
 	if !ok {
-		return nil, false
+		return Anchor{}, false
 	}
 
 	children, err := res.Children()
 	if err != nil {
 		h.err = err
-		return nil, false
+		return Anchor{}, false
 	}
 
 	a, err := children.At(i)
 	if err != nil {
 		h.err = err
-		return nil, false
+		return Anchor{}, false
 	}
 
 	return Anchor(a), true
@@ -109,53 +108,6 @@ func (h *handler) results() (res api.Anchor_ls_Results, ok bool) {
 	}
 
 	return
-}
-
-// type Iterator struct {
-// 	Err  error
-// 	Name string
-// 	pos  int
-// 	cs   api.Anchor_Child_List
-// }
-
-// func newIterator(cs api.Anchor_Child_List) *Iterator {
-// 	return &Iterator{cs: cs}
-// }
-
-// func newErrIterator(err error) *Iterator {
-// 	return &Iterator{Err: err}
-// }
-
-// func (rs *Iterator) More() bool {
-// 	return rs.Err == nil && rs.pos < rs.cs.Len()
-// }
-
-// func (rs *Iterator) Next() (more bool) {
-// 	if more = rs.More(); more {
-// 		rs.Name, rs.Err = rs.cs.At(rs.pos).Name()
-// 		rs.pos++
-// 	}
-
-// 	return
-// }
-
-// func (rs *Iterator) Anchor() Anchor {
-// 	return Anchor(rs.cs.At(rs.pos).Anchor())
-// }
-
-/*
-
-	Generic methods for client implementations
-
-*/
-
-func walkPath(ctx context.Context, a api.Anchor, path Path) (Client, capnp.ReleaseFunc) {
-	if path.IsRoot() {
-		return Client(a), func() {}
-	}
-
-	f, release := a.Walk(ctx, walkParam(path))
-	return Client(f.Anchor()), release
 }
 
 func walkParam(path Path) func(api.Anchor_walk_Params) error {
