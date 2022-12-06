28b5f9b47bcd63bc65028b9f6e29ee7acdec0109
Evan <evan.schott@yale.edu>
2022-12-06 12:07:57 -0500

    make sure setup() called before teardown()

--- internal/cmd/cluster/cluster.go
+++ internal/cmd/cluster/cluster.go
@@ -26,38 +26,42 @@ var subcommands = []*cli.Command{
 	// Discover(),
 }
 
+var flags = []cli.Flag{
+	&cli.StringSliceFlag{
+		Name:    "addr",
+		Aliases: []string{"a"},
+		Usage:   "static bootstrap `ADDR`",
+		EnvVars: []string{"WW_ADDR"},
+	},
+	&cli.StringFlag{
+		Name:    "discover",
+		Aliases: []string{"d"},
+		Usage:   "bootstrap discovery `ADDR`",
+		Value:   "/ip4/228.8.8.8/udp/8822/multicast/lo0",
+		EnvVars: []string{"WW_DISCOVER"},
+	},
+	&cli.StringFlag{
+		Name:    "ns",
+		Usage:   "cluster namespace",
+		Value:   "ww",
+		EnvVars: []string{"WW_NS"},
+	},
+	&cli.DurationFlag{
+		Name:    "timeout",
+		Usage:   "dial timeout",
+		Value:   time.Second * 15,
+		EnvVars: []string{"WW_CLIENT_TIMEOUT"},
+	},
+}
+
 func Command() *cli.Command {
 	return &cli.Command{
-		Name:    "cluster",
-		Usage:   "cli client for wetware clusters",
-		Aliases: []string{"client"}, // TODO(soon):  deprecate
-		Flags: []cli.Flag{
-			&cli.StringSliceFlag{
-				Name:    "addr",
-				Aliases: []string{"a"},
-				Usage:   "static bootstrap `ADDR`",
-				EnvVars: []string{"WW_ADDR"},
-			},
-			&cli.StringFlag{
-				Name:    "discover",
-				Aliases: []string{"d"},
-				Usage:   "bootstrap discovery `ADDR`",
-				Value:   "/ip4/228.8.8.8/udp/8822/multicast/lo0",
-				EnvVars: []string{"WW_DISCOVER"},
-			},
-			&cli.StringFlag{
-				Name:    "ns",
-				Usage:   "cluster namespace",
-				Value:   "ww",
-				EnvVars: []string{"WW_NS"},
-			},
-			&cli.DurationFlag{
-				Name:    "timeout",
-				Usage:   "dial timeout",
-				Value:   time.Second * 15,
-				EnvVars: []string{"WW_CLIENT_TIMEOUT"},
-			},
-		},
+		Name:        "cluster",
+		Usage:       "cli client for wetware clusters",
+		Aliases:     []string{"client"}, // TODO(soon):  deprecate
+		Flags:       flags,
+		Before:      setup(),
+		After:       teardown(),
 		Subcommands: subcommands,
 	}
 }
@@ -83,14 +87,10 @@ func setup() cli.BeforeFunc {
 
 func teardown() cli.AfterFunc {
 	return func(c *cli.Context) (err error) {
-		// if app != nil {
 		ctx, cancel := context.WithTimeout(
 			context.Background(),
 			app.StopTimeout())
 		defer cancel()
 		return app.Stop(ctx)
-		//err = app.Stop(ctx)
-		//}
-		//return
 	}
 }
