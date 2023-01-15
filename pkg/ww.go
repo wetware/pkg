dd5232ade44ebf5039b207e32eb3467f8af65012
Louis Thibault <l.thibault@sentimens.com>
2023-01-14 23:21:57 -0500

    Bump version to v0.0.1-beta.29

--- pkg/ww.go
+++ pkg/ww.go
@@ -6,7 +6,7 @@ import (
 	protoutil "github.com/wetware/casm/pkg/util/proto"
 )
 
-const Version = "0.0.1-beta.28"
+const Version = "0.0.1-beta.29"
 
 var match = casm.NewMatcher("ww").
 	Then(protoutil.SemVer(Version))
