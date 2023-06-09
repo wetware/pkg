e0cf52f93d9eb0bfde167755753c1a1c3c1b8fb4
Louis Thibault <l.thibault@sentimens.com>
2023-06-09 14:19:34 -0400

    Add bitswap server & client capability.

---
+++ pkg/bitswap/bitswap_test.go
@@ -0,0 +1,95 @@
+package bitswap_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-cid"
+	"github.com/stretchr/testify/require"
+	mock_bitswap "github.com/wetware/ww/internal/mock/pkg/bitswap"
+	"github.com/wetware/ww/pkg/bitswap"
+)
+
+var (
+	matchCtx = gomock.AssignableToTypeOf(context.Background())
+	matchCID = gomock.AssignableToTypeOf(cid.Cid{})
+)
+
+func TestBitSwap(t *testing.T) {
+	t.Parallel()
+	t.Helper()
+
+	t.Run("Succeed", func(t *testing.T) {
+		t.Parallel()
+
+		/*
+			Test the "happy path".
+		*/
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		want := blocks.NewBlock([]byte("hello, world!"))
+
+		ex := mock_bitswap.NewMockExchange(ctrl)
+		ex.EXPECT().
+			GetBlock(matchCtx, matchCID).
+			Return(want, nil).
+			Times(1)
+
+		bs := bitswap.Server{Exchange: ex}.BitSwap()
+		got, err := bs.GetBlock(context.Background(), want.Cid())
+		require.NoError(t, err)
+		require.True(t, got.Cid().Equals(want.Cid()),
+			"CIDs should match")
+	})
+
+	t.Run("ErrWrongHash", func(t *testing.T) {
+		t.Parallel()
+
+		/*
+			Test that we fail if the block returned by the client does not
+			correspond to the CID we requested.
+		*/
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		want := blocks.NewBlock([]byte("hello, world!"))
+		got := blocks.NewBlock([]byte("something else"))
+
+		ex := mock_bitswap.NewMockExchange(ctrl)
+		ex.EXPECT().
+			GetBlock(matchCtx, matchCID).
+			Return(got, nil).
+			Times(1)
+
+		bs := bitswap.Server{Exchange: ex}.BitSwap()
+
+		_, err := bs.GetBlock(context.Background(), want.Cid())
+		require.ErrorIs(t, err, blocks.ErrWrongHash,
+			"should fail due to hash mismatch")
+	})
+
+	t.Run("ErrInvalidKey", func(t *testing.T) {
+		t.Parallel()
+
+		/*
+			Test that we fail if the zero-value cid.Cid{} is supplied as the key.
+			This test also checks that the failure happens before any RPC data
+			is written to the wire.
+		*/
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		ex := mock_bitswap.NewMockExchange(ctrl)
+		bs := bitswap.Server{Exchange: ex}.BitSwap()
+
+		got, err := bs.GetBlock(context.Background(), cid.Cid{}) // oops!
+		require.ErrorIs(t, err, cid.ErrInvalidCid{})
+		require.Nil(t, got, "should not return block")
+	})
+}
