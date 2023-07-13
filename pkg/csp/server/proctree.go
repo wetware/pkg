be2adcadd46a9f37361d298949e9380039bc0f84
mikelsr <mikelsr@pm.me>
2023-07-14 01:33:23 +0200

    Optimize findParents

--- pkg/csp/server/proctree.go
+++ pkg/csp/server/proctree.go
@@ -8,40 +8,45 @@ type ProcTree struct {
 
 // func (pt ProcTree) Pop(id uint32) *Node {
 
-// }
+// FindParent returns the parent of the process with PID=pid. nil if not found.
+func (pt ProcTree) FindParent(pid uint32) *ProcNode {
+	n, _ := findParent(pt.Root, pid)
+	return n
+}
 
 func (pt ProcTree) FindParent(id uint32) *ProcNode {
 	n, _ := findParent(pt.Root, id)
 	return n
 }
 
+// findParent does a Depth First Search of the tree until
+// finding the node with PID=pid, then returns it's parent node.
 func findParent(n *ProcNode, pid uint32) (*ProcNode, bool) {
-	// child is first node to the left, immediate child
+	// Corner case, defaults to being the right-branch node.
+	if n == nil || n.Pid == pid {
+		return nil, n != nil
+	}
+
+	// Child is first node to the left, immediate child.
 	if n.Left != nil && n.Left.Pid == pid {
 		return n, false
 	}
 
 	if n.Left != nil {
 		x, childInRight := findParent(n.Left, pid)
-		// node was a children or grandchildren
+		// Node was a children or grandchildren.
 		if x != nil {
 			return x, false
 		} else {
-			// node was a sibling of right
+			// Node was a sibling of right.
 			if childInRight {
 				return n, false
 			}
 		}
 	}
 
-	// immeadiate sibling had the id
-	if n.Right != nil && n.Right.Pid == pid {
-		return nil, true
-	}
-
-	// explore immediate sibling
-	if n.Right != nil {
-		return findParent(n.Right, pid)
+	// Explore immediate sibling.
+	return findParent(n.Right, pid)
 	}
 	return nil, false
 }
