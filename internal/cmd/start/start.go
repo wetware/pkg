5e944b8f3a0398d401707de469f2fca6112d8d86
Louis Thibault <l.thibault@sentimens.com>
2023-07-27 09:02:05 -0400

    Update start command to use new server package.

--- internal/cmd/start/start.go
+++ internal/cmd/start/start.go
@@ -2,108 +2,96 @@ package start
 
 import (
 	"context"
+	"fmt"
 	"path"
 	"runtime"
+	"strings"
 
 	"github.com/lthibault/log"
-
 	"github.com/urfave/cli/v2"
-	"go.uber.org/fx"
-
-	ww_runtime "github.com/wetware/ww/pkg/runtime"
+	"github.com/wetware/ww/server"
 )
 
-var (
-	app    *fx.App
-	logger log.Logger
-)
-
-var flags = []cli.Flag{
-	&cli.StringFlag{
-		Name:    "ns",
-		Usage:   "cluster namespace",
-		Value:   "ww",
-		EnvVars: []string{"WW_NS"},
-	},
-	&cli.StringSliceFlag{
-		Name:    "listen",
-		Aliases: []string{"l"},
-		Usage:   "host listen address",
-		Value: cli.NewStringSlice(
-			"/ip4/0.0.0.0/udp/0/quic",
-			"/ip6/::0/udp/0/quic"),
-		EnvVars: []string{"WW_LISTEN"},
-	},
-	&cli.StringSliceFlag{
-		Name:    "addr",
-		Aliases: []string{"a"},
-		Usage:   "static bootstrap `ADDR`",
-		EnvVars: []string{"WW_ADDR"},
-	},
-	&cli.StringFlag{
-		Name:    "discover",
-		Aliases: []string{"d"},
-		Usage:   "bootstrap discovery multiaddr",
-		Value:   bootstrapAddr(),
-		EnvVars: []string{"WW_DISCOVER"},
-	},
-	&cli.StringSliceFlag{
-		Name:    "meta",
-		Usage:   "metadata fields in key=value format",
-		EnvVars: []string{"WW_META"},
-	},
-}
+var meta map[string]string
 
 // Command constructor
 func Command() *cli.Command {
 	return &cli.Command{
-		Name:   "start",
-		Usage:  "start a host process",
-		Flags:  flags,
-		Before: setup(),
-		Action: serve(),
-		After:  teardown(),
+		Name:  "start",
+		Usage: "start a host process",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "ns",
+				Usage:   "cluster namespace",
+				Value:   "ww",
+				EnvVars: []string{"WW_NS"},
+			},
+			&cli.StringSliceFlag{
+				Name:    "listen",
+				Aliases: []string{"l"},
+				Usage:   "host listen address",
+				Value: cli.NewStringSlice(
+					"/ip4/0.0.0.0/udp/0/quic",
+					"/ip6/::0/udp/0/quic"),
+				EnvVars: []string{"WW_LISTEN"},
+			},
+			&cli.StringSliceFlag{
+				Name:    "join",
+				Aliases: []string{"j"},
+				Usage:   "join cluster via existing peer `ADDR`",
+				EnvVars: []string{"WW_JOIN"},
+			},
+			&cli.StringFlag{
+				Name:    "discover",
+				Aliases: []string{"d"},
+				Usage:   "multiaddr of peer-discovery service",
+				Value:   bootstrapAddr(),
+				EnvVars: []string{"WW_DISCOVER"},
+			},
+			&cli.StringSliceFlag{
+				Name:    "meta",
+				Usage:   "metadata fields in key=value format",
+				EnvVars: []string{"WW_META"},
+			},
+		},
+		Before: bindMeta(),
+		Action: func(c *cli.Context) error {
+			config := server.Config{
+				Logger:   log.New(),
+				NS:       c.String("ns"),
+				Join:     c.StringSlice("join"),
+				Discover: c.String("discover"),
+				Meta:     meta,
+			}
+
+			err := config.ListenAndServe(c.Context, c.StringSlice("listen")...)
+			if err != context.Canceled {
+				return err
+			}
+
+			return nil
+		},
 	}
 }
 
-func setup() cli.BeforeFunc {
+func bindMeta() cli.BeforeFunc {
 	return func(c *cli.Context) error {
-		app = fx.New(
-			ww_runtime.NewServer(c.Context, c),
-			fx.Populate(&logger))
+		metaTags := c.StringSlice("meta")
 
-		return start(c.Context, app)
-	}
-}
+		for _, tag := range metaTags {
+			pair := strings.SplitN(tag, "=", 2)
+			if len(pair) != 2 {
+				return fmt.Errorf("invalid meta tag: %s", tag)
+			}
 
-func serve() cli.ActionFunc {
-	return func(*cli.Context) error {
-		logger.Info("wetware started")
+			if meta == nil {
+				meta = make(map[string]string, len(metaTags))
+			}
 
-		signal := <-app.Done()
-		logger.
-			WithField("signal", signal).
-			Warn("shutdown signal received")
+			meta[pair[0]] = pair[1]
+		}
 
 		return nil
-	}
-}
-
-func start(ctx context.Context, app *fx.App) error {
-	ctx, cancel := context.WithTimeout(ctx, app.StartTimeout())
-	defer cancel()
-
-	return app.Start(ctx)
-}
-
-func teardown() cli.AfterFunc {
-	return func(c *cli.Context) error {
-		ctx, cancel := context.WithTimeout(
-			context.Background(),
-			app.StopTimeout())
-		defer cancel()
-
-		return app.Stop(ctx)
 	}
 }
 
