bedbd4798a301e08b64f8630083dc36f56f4acfa
Louis Thibault <l.thibault@sentimens.com>
2023-07-27 13:01:47 -0400

    Use top-level client/ package in run command. Clean up CLI.

--- cmd/ww/main.go
+++ cmd/ww/main.go
@@ -11,6 +11,8 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"path"
+	"runtime"
 	"syscall"
 
 	"github.com/urfave/cli/v2"
@@ -19,6 +21,42 @@ import (
 	cmdrun "github.com/wetware/ww/internal/cmd/run"
 	cmdstart "github.com/wetware/ww/internal/cmd/start"
 )
+
+var flags = []cli.Flag{
+	&cli.StringFlag{
+		Name:    "ns",
+		Usage:   "cluster namespace",
+		Value:   "ww",
+		EnvVars: []string{"WW_NS"},
+	},
+	&cli.StringSliceFlag{
+		Name:    "peer",
+		Aliases: []string{"p"},
+		Usage:   "bootstrap peer `ADDR`",
+		EnvVars: []string{"WW_PEERS"},
+	},
+	&cli.StringFlag{
+		Name:    "discover",
+		Aliases: []string{"d"},
+		Usage:   "use discovery service",
+		Value:   bootstrapAddr(),
+		EnvVars: []string{"WW_DISCOVER"},
+	},
+
+	// Category:  Logging
+	&cli.BoolFlag{
+		Name:     "debug",
+		Usage:    "enable debug logging",
+		EnvVars:  []string{"WW_DEBUG"},
+		Category: "LOGGING",
+	},
+	&cli.BoolFlag{
+		Name:     "json",
+		Usage:    "enable json logging",
+		EnvVars:  []string{"WW_JSON"},
+		Category: "LOGGING",
+	},
+}
 
 func main() {
 	ctx, cancel := signal.NotifyContext(
@@ -36,6 +74,7 @@ func main() {
 		UsageText:            "ww [global options] command [command options] [arguments...]",
 		Copyright:            "2020 The Wetware Project",
 		EnableBashCompletion: true,
+		Flags:                flags,
 		Commands: []*cli.Command{
 			cmdrun.Command(),
 			cmdstart.Command(),
@@ -45,5 +84,17 @@ func main() {
 	if err := app.RunContext(ctx, os.Args); err != nil {
 		fmt.Println(err)
 		os.Exit(1) // application error
+	}
+}
+func bootstrapAddr() string {
+	return path.Join("/ip4/228.8.8.8/udp/8822/multicast", loopback())
+}
+
+func loopback() string {
+	switch runtime.GOOS {
+	case "darwin":
+		return "lo0"
+	default:
+		return "lo"
 	}
 }
