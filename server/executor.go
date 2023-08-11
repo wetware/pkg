6c93c71f3dd5fc5b13540751d3cf6d61535957ff
mikelsr <mikelsr@pm.me>
2023-08-11 12:15:49 +0200

    Provide executor to cluster

---
+++ server/executor.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"context"
+
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
+
+	csp_server "github.com/wetware/pkg/cap/csp/server"
+)
+
+type executorConfig struct {
+	RuntimeCfg wazero.RuntimeConfig
+}
+
+func (cfg Config) newExecutor(ctx context.Context, ec executorConfig) (csp_server.Runtime, error) {
+	r := wazero.NewRuntimeWithConfig(ctx, ec.RuntimeCfg)
+	_, err := wasi_snapshot_preview1.Instantiate(ctx, r)
+	if err != nil {
+		return csp_server.Runtime{}, err
+	}
+
+	return csp_server.Runtime{
+		Runtime: r,
+	}, nil
+}
