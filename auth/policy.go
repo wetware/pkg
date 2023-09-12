7b57e4e34970dd6578da505c54d15e3c6549ac54
Louis Thibault <l.thibault@sentimens.com>
2023-09-12 18:33:12 -0400

    Add auth.OnlyAllow policy.

--- auth/policy.go
+++ auth/policy.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/libp2p/go-libp2p/core/peer"
@@ -18,6 +19,22 @@ type Policy func(context.Context, SessionSetter, Session, peer.ID) error
 func AllowAll(ctx context.Context, res SessionSetter, root Session, account peer.ID) error {
 	sess := api.Session(root.AddRef())
 	return res.SetSession(sess)
+}
+
+func OnlyAllow(allow ...peer.ID) Policy {
+	allowed := make(map[peer.ID]any, len(allow))
+	for _, id := range allow {
+		allowed[id] = struct{}{}
+	}
+
+	return func(ctx context.Context, res SessionSetter, root Session, account peer.ID) error {
+		// account not in the set of allowed accounts?
+		if allowed[account] == nil {
+			return errors.New("denied")
+		}
+
+		return AllowAll(ctx, res, root, account) // success
+	}
 }
 
 func Deny(reason string, args ...any) Policy {
