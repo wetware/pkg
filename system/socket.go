c882a03467c204c7be10ff2e07882ba87db93c7f
Louis Thibault <l.thibault@sentimens.com>
2023-09-20 18:50:23 -0400

    Add basic socket implementation.

--- system/socket.go
+++ system/socket.go
@@ -2,23 +2,32 @@ package system
 
 import (
 	"context"
-	"errors"
-	"log/slog"
-	"net"
 
 	"capnproto.org/go/capnp/v3"
 	"capnproto.org/go/capnp/v3/rpc"
+	rpccp "capnproto.org/go/capnp/v3/std/capnp/rpc"
+	"zenhack.net/go/util/rc"
+
+	"github.com/stealthrocket/wazergo"
 	"github.com/stealthrocket/wazergo/types"
+
 	"github.com/wetware/pkg/api/core"
 	"github.com/wetware/pkg/auth"
 	"github.com/wetware/pkg/util/log"
 )
 
+// module for wetware Host
+var module wazergo.HostModule[*Socket] = functions{
+	"__sock_recv":  wazergo.F1((*Socket).Recv),
+	"__sock_send":  wazergo.F1((*Socket).Send),
+	"__sock_close": wazergo.F0((*Socket).close),
+}
+
 // Socket is a system socket that uses the host's IP stack.
 type Socket struct {
-	Logger  log.Logger
-	Conn    net.Conn
-	Session auth.Session
+	Logger      log.Logger
+	Host, Guest *Pipe
+	Session     auth.Session
 
 	conn *rpc.Conn
 }
@@ -43,20 +52,48 @@ func (sock *Socket) dial(ctx context.Context) error {
 	server := core.Terminal_NewServer(sock.Session)
 	client := capnp.NewClient(server)
 
-	sock.conn = rpc.NewConn(rpc.NewStreamTransport(sock.Conn), &rpc.Options{
+	transport := hostTransport{sock}
+
+	options := &rpc.Options{
 		ErrorReporter:   ErrorReporter{Logger: sock.Logger},
 		BootstrapClient: client,
-	})
+	}
 
+	sock.conn = rpc.NewConn(transport, options)
 	return nil
 }
 
-func (sock *Socket) Send(ctx context.Context, b types.Bytes) types.Error {
-	slog.Warn("STUB CALL TO Socket.Send()")
-	return types.Fail(errors.New("NOT IMPLEMENTED"))
+func (sock *Socket) Send(ctx context.Context, buf types.Pointer[types.Bytes]) types.Error {
+	msg, err := capnp.Unmarshal(buf.Load())
+	if err != nil {
+		return types.Fail(err)
+	}
+
+	message, err := rpccp.ReadRootMessage(msg)
+	if err != nil {
+		return types.Fail(err)
+	}
+
+	ref := rc.NewRef(message, msg.Release)
+
+	if err = sock.Host.Push(ref); err != nil {
+		return types.Fail(err)
+	}
+
+	return types.OK
 }
 
-func (sock *Socket) Recv(ctx context.Context, b types.Bytes) types.Error {
-	slog.Warn("STUB CALL TO Socket.Recv()")
-	return types.Fail(errors.New("NOT IMPLEMENTED"))
+func (sock *Socket) Recv(ctx context.Context, buf types.Pointer[types.Bytes]) types.Error {
+	ref, err := sock.Guest.Pop()
+	if err != nil {
+		return types.Fail(err)
+	}
+
+	b, err := ref.Value().Message().Marshal()
+	if err != nil {
+		return types.Fail(err)
+	}
+	buf.Store(b)
+
+	return types.OK
 }
