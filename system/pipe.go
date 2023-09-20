d80bcbdd9447e11abd7788252202625084b8cce0
Louis Thibault <l.thibault@sentimens.com>
2023-09-20 18:48:59 -0400

    Add basic buffered pipe + unit tests.

---
+++ system/pipe.go
@@ -0,0 +1,60 @@
+package system
+
+import (
+	"context"
+	"errors"
+
+	rpccp "capnproto.org/go/capnp/v3/std/capnp/rpc"
+	"zenhack.net/go/util/rc"
+)
+
+func NewPipe() *Pipe {
+	return &Pipe{
+		closed: make(chan struct{}),
+		buffer: make(chan *rc.Ref[rpccp.Message], 1),
+	}
+}
+
+type Pipe struct {
+	closed chan struct{}
+	buffer chan *rc.Ref[rpccp.Message]
+}
+
+func (p *Pipe) Push(ref *rc.Ref[rpccp.Message]) error {
+	select {
+	case p.buffer <- ref:
+		// fast path; we have a message waiting in the buffer
+		return nil
+
+	case <-p.closed:
+		return errors.New("closed")
+
+	default:
+		return context.DeadlineExceeded // means "please back off"
+	}
+}
+
+func (p *Pipe) Pop() (*rc.Ref[rpccp.Message], error) {
+	select {
+	case ref := <-p.buffer:
+		return ref, nil
+
+	case <-p.closed:
+		return nil, errors.New("closed")
+
+	default:
+		return nil, context.DeadlineExceeded
+	}
+}
+
+// Close closes the connection.
+// Any blocked Read or Write operations will be unblocked and return errors.
+func (p *Pipe) Close() error {
+	select {
+	case <-p.closed:
+		return errors.New("closed")
+	default:
+		close(p.closed)
+		return nil
+	}
+}
---
+++ system/pipe_test.go
@@ -0,0 +1,64 @@
+package system_test
+
+import (
+	"context"
+	"testing"
+
+	rpccp "capnproto.org/go/capnp/v3/std/capnp/rpc"
+	"github.com/stretchr/testify/require"
+	"github.com/wetware/pkg/system"
+	"zenhack.net/go/util/rc"
+)
+
+func TestPipe(t *testing.T) {
+	t.Parallel()
+	t.Helper()
+
+	t.Run("Empty", func(t *testing.T) {
+		t.Parallel()
+
+		pipe := system.NewPipe()
+
+		want := new(rc.Ref[rpccp.Message])
+		defer want.Release()
+
+		err := pipe.Push(want)
+		require.NoError(t, err, "push should succeed")
+
+		got, err := pipe.Pop()
+		require.NoError(t, err, "pop should succeed")
+		require.Equal(t, want, got, "should get expected item from the queue")
+	})
+
+	t.Run("Full", func(t *testing.T) {
+		t.Parallel()
+
+		pipe := system.NewPipe()
+
+		want := new(rc.Ref[rpccp.Message])
+		defer want.Release()
+
+		err := pipe.Push(want)
+		require.NoError(t, err, "push should succeed")
+
+		err = pipe.Push(want)
+		require.ErrorIs(t, err, context.DeadlineExceeded,
+			"should signal that buffer is full")
+	})
+
+	t.Run("Closed", func(t *testing.T) {
+		t.Parallel()
+
+		pipe := system.NewPipe()
+
+		err := pipe.Close()
+		require.NoError(t, err, "should close successfully")
+
+		err = pipe.Push(nil)
+		require.EqualError(t, err, "closed", "pipe should be closed")
+
+		ref, err := pipe.Pop()
+		require.EqualError(t, err, "closed", "pipe should be closed")
+		require.Nil(t, ref, "should not return ref")
+	})
+}
