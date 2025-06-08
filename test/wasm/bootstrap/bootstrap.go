19a3d7cfbd19ee3ddcd06ef4db3882731bf4eee4
mikelsr <mikelsr@pm.me>
2025-06-08 23:28:41 +0200

    Update Bootstrap guest example to use the bootstrap capability

--- test/wasm/bootstrap/bootstrap.go
+++ test/wasm/bootstrap/bootstrap.go
@@ -6,26 +6,50 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/wetware/pkg/guest/system"
+	ww "github.com/wetware/pkg/guest/system"
 )
 
 func main() {
 	ctx := context.Background()
 
-	caps, err := system.Bootstrap(ctx)
+	caps, releases, err := ww.Bootstrap(ctx)
 	if err != nil {
 		panic(err)
 	}
+	defer func() {
+		for _, release := range releases {
+			release()
+		}
+	}()
 
 	if len(caps) == 0 {
 		fmt.Println("No capabilities found in bootstrap")
 		os.Exit(1)
 	}
 
-	session, err := system.Login(ctx, caps[0])
+	session, err := ww.Login(ctx, caps[0])
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Successfully bootstrapped session %v\n", session)
+	processes, release, err := session.Exec().Ps(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer release()
+
+	selfFound := false
+	pid := ww.Pid()
+	for _, proc := range processes {
+		if proc.Pid() == pid {
+			selfFound = true
+			break
+		}
+	}
+	if !selfFound {
+		fmt.Println("Self process not found in the list of processes")
+		os.Exit(1)
+	}
+	fmt.Println("Self found successfully")
 }
